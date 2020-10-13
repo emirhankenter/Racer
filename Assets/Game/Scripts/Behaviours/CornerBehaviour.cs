a8a8be112e1fee017ebf34a37fa94da7115a97c8
Emirhan Kenter <emirhankenter@gmail.com>
2020-10-13 23:05:41 +0300

    basic rotation/movement

---
+++ Assets/Game/Scripts/Behaviours/CornerBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Scripts.Behaviours
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class CornerBehaviour : MonoBehaviour
+    {
+        [SerializeField] private Transform _anchorPoint;
+        public Transform AnchorPoint => _anchorPoint;
+
+    }
+}
--- Assets/Game/Scripts/Behaviours/PlayerBehaviour.cs
+++ Assets/Game/Scripts/Behaviours/PlayerBehaviour.cs
@@ -9,7 +9,13 @@ namespace Game.Scripts.Behaviours
 {
     public class PlayerBehaviour : MonoBehaviour
     {
+        [SerializeField] Rigidbody _rigidBody;
+
+        protected float Speed => 150f;
+
         private bool _holding;
+        private bool _canMove;
+        private bool _canRotate;
 
         protected PlayerInputs Input;
         private void Awake()
@@ -19,12 +25,16 @@ namespace Game.Scripts.Behaviours
             Input.Enable();
 
             RegisterEvents();
+
+            _canMove = true;
+            StartCoroutine(MoveRoutine());
         }
 
         private void OnDestroy()
         {
             Input.Disable();
 
+            _canMove = false;
             UnregisterEvents();
         }
 
@@ -40,6 +50,10 @@ namespace Game.Scripts.Behaviours
             Input.Player.Press.canceled -= OnPressCanceled;
         }
 
+        private void FixedUpdate()
+        {
+        }
+
         private void OnPressPerformed(InputAction.CallbackContext obj)
         {
             _holding = true;
@@ -47,6 +61,7 @@ namespace Game.Scripts.Behaviours
             Debug.Log("PressPerformed");
 
             StartCoroutine(Holding());
+            StartCoroutine(MoveRoutine());
         }
         private void OnPressCanceled(InputAction.CallbackContext obj)
         {
@@ -55,12 +70,53 @@ namespace Game.Scripts.Behaviours
             Debug.Log("PressCanceled");
         }
 
+        private IEnumerator MoveRoutine()
+        {
+            while (_canMove)
+            {
+                _rigidBody.MovePosition(Vector3.Lerp(transform.position, transform.position + transform.forward, Time.fixedDeltaTime * Speed));
+                yield return new WaitForFixedUpdate();
+            }
+        }
+
         private IEnumerator Holding()
         {
             while (_holding)
             {
                 yield return new WaitForFixedUpdate();
             }
+        }
+
+        private IEnumerator Rotating()
+        {
+            while (_canRotate)
+            {
+                _rigidBody.MoveRotation(_rigidBody.rotation * Quaternion.Euler(new Vector3(0, 120, 0) * Time.fixedDeltaTime));
+                yield return new WaitForFixedUpdate();
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.gameObject.CompareTag("Corner")) return;
+
+            var corner = other.GetComponent<CornerBehaviour>();
+
+            transform.SetParent(corner.AnchorPoint, true);
+
+            _canRotate = true;
+            StartCoroutine(Rotating());
+
+            Debug.Log("Entered");
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!other.gameObject.CompareTag("Corner")) return;
+            _canRotate = false;
+            transform.SetParent(null);
+
+            Debug.Log("Exited");
         }
     }
 }
